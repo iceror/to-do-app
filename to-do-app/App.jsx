9c8e6e17c600e849639d7920429f89e1fcac7a53
iceror <ilsecervor@gmail.com>
2024-01-12 16:30:03 -0600

    add first elements

---
+++ to-do-app/App.jsx
@@ -0,0 +1,7 @@
+export const App = () => {
+  return (
+    <header>
+      <h1>To-do App</h1>
+    </header>
+  )
+}
--- to-do-app/main.jsx
+++ to-do-app/main.jsx
@@ -1,7 +1,8 @@
 import { createRoot } from 'react-dom/client';
+import { App } from './App';
 
 const root = createRoot(document.getElementById('root'));
 
 root.render(
-  <h2>hola</h2>
+  <App/>
 )
