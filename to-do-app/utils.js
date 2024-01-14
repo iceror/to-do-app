92c3e8830473b3d421f2954247b7e0f73eec4728
iceror <ilsecervor@gmail.com>
2024-01-14 16:07:02 -0600

    declar api functions

--- to-do-app/utils.js
+++ to-do-app/utils.js
@@ -17,3 +17,46 @@ export const getTasks = () => {
       console.log(error);
     });
 }
+
+export const getTask = (id) => {
+  let config = {
+    method: 'get',
+    maxBodyLength: Infinity,
+    url: `http://localhost:3000/tasks/${id}`,
+    headers: { }
+  };
+  
+  return axios.request(config)
+  .then((response) => {
+    // console.log(JSON.stringify(response.data));
+    return response.data;
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+}
+
+export const patchTask = (id, status) => {
+  let data = JSON.stringify({
+    "status": status
+  });
+  
+  let config = {
+    method: 'patch',
+    maxBodyLength: Infinity,
+    url: `http://localhost:3000/tasks/${id}`,
+    headers: { 
+      'Content-Type': 'application/json'
+    },
+    data : data
+  };
+  
+  return axios.request(config)
+  .then((response) => {
+    // console.log(JSON.stringify(response.data));
+    return response.data;
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+}
