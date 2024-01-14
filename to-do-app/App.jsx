aac36091b5e1215709c8f50d0fc60f4fb7fb3e84
iceror <ilsecervor@gmail.com>
2024-01-14 16:06:26 -0600

    Update App.jsx

--- to-do-app/App.jsx
+++ to-do-app/App.jsx
@@ -1,7 +1,26 @@
+import { useEffect, useState } from "react";
+import { Tasks } from "./Components/Tasks";
+import { getTasks } from "./utils";
+
 export const App = () => {
+  const [tasks, setTasks] = useState([]);
+  const fetchTasks = async () => {
+    setTasks(await getTasks())
+  }
+  console.log(tasks);
+
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+
   return (
-    <header>
-      <h1>To-do App</h1>
-    </header>
+    <>
+      <header>
+        <h1>To-do App</h1>
+      </header>
+      <main>
+        <Tasks tasks={tasks} />
+      </main>
+    </>
   )
 }
