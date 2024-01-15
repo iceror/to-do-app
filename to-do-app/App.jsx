42277fff51ec48c9c3a9022cd1633a93e341331e
iceror <ilsecervor@gmail.com>
2024-01-14 22:58:36 -0600

    change between pending, completed && deleted tasks

--- to-do-app/App.jsx
+++ to-do-app/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { Tasks } from "./Components/Tasks";
 import { getTask, getTasks, postTask, deleteTask, patchTask } from "./utils";
-import { Button } from "@mui/material";
+import { Button, ButtonGroup } from "@mui/material";
 
 export const App = () => {
   const [tasks, setTasks] = useState([]);
@@ -9,6 +9,7 @@ export const App = () => {
   const [currentTitle, setCurrentTitle] = useState('');
   const [showInput, setShowInput] = useState(false);
   const [showDeletedTasks, setShowDeletedTasks] = useState(false);
+  const [showCompletedTasks, setShowCompletedTasks] = useState(false);
 
   const fetchTasks = async () => {
     setTasks(await getTasks());
@@ -57,18 +58,27 @@ export const App = () => {
         <h1>To-do App</h1>
       </header>
       <main>
+
+        <ButtonGroup variant="contained" aria-label="outlined primary button group">
+          <Button variant="contained" onClick={() => { setShowCompletedTasks(false); setShowDeletedTasks(false) }}>Pending tasks</Button>
+          <Button variant="contained" onClick={() => { setShowCompletedTasks(true); setShowDeletedTasks(false) }}>Completed tasks</Button>
+          <Button variant="contained" onClick={() => setShowDeletedTasks(true)}>Deleted tasks</Button>
+        </ButtonGroup>
+
         <Button variant='contained' onClick={() => setShowInput(!showInput)}>Add</Button>
-        {showDeletedTasks === false ?
-          <Button variant="contained" onClick={() => setShowDeletedTasks(true)}>Deleted tasks</Button> :
-          <Button variant="contained" onClick={() => setShowDeletedTasks(false)}>Pending tasks</Button>
-        }
         {showInput ?
           <form action="">
             <input type="text" onChange={handleChange} onKeyDown={handleSaveTask} />
           </form>
           : null}
-        <Tasks tasks={!showDeletedTasks ? tasks.filter((task) => task.status === 'pending' || task.status === 'completed') : tasks.filter((task) => task.status === 'deleted')}
-          fetchTask={fetchTask} currentTitle={currentTitle} handleDelete={handleDelete} handleRetrieve={handleRetrieve}/>
+        <Tasks tasks={
+          !showDeletedTasks
+            ? showCompletedTasks
+              ? tasks.filter((task) => task.status === 'completed')
+              : tasks.filter((task) => task.status === 'pending')
+            : tasks.filter((task) => task.status === 'deleted')
+        }
+          fetchTask={fetchTask} currentTitle={currentTitle} handleDelete={handleDelete} handleRetrieve={handleRetrieve} />
       </main>
     </>
   )
