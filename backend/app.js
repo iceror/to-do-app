580821ab071f11fcb965e14479de1e05e9671152
iceror <ilsecervor@gmail.com>
2024-01-16 14:41:18 -0600

    set a port for backend

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 app.listen(PORT, () => {
   console.log('App listening in port: ', PORT);
